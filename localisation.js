3610fd4c6311a57f8ec98850797d490ec7cfd600
AzizBENJEMMAA <aziz.benjemaa13034544@gmail.com>
2025-04-15 11:36:48 +0100

    Add files via upload

---
+++ localisation.js
@@ -0,0 +1,9 @@
+<script>
+  function openModal() {
+    document.getElementById("cityModal").style.display = "block" ;
+  }
+
+  function closeModal() {
+    document.getElementById("cityModal").style.display = "none" ;
+  }
+</script>
